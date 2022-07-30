53f28270b29ae16c5c4bd0dffd4d62627787ee79
Geandson Nunes <92594492+geandson14@users.noreply.github.com>
2022-07-29 22:43:33 -0300

    29/07/2022

--- js/script.js
+++ js/script.js
@@ -29,3 +29,20 @@ $(function(){
         startDelay: 1000
     });
 })
+
+const skills = document.querySelectorAll('.hab ul li');
+const descricao = document.querySelector('.desc');
+const sobreSkill = [
+    '<p>HTML</p> <br> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p> <br>',
+    '<p>CSS</p> <br> <p>É um mecanismo para adicionar estilo a um documento web.</p> <br>',
+    '<p>JavaScript</p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
+    '<p>Styled Components</p> <br> <p>É uma biblioteca (lib) que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript.</p> <br>',
+    '<p>Git</p> <br> <p>É um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.</p> <br>',
+    '<p>Github</p> <br> <p>É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>'
+]
+
+skills.forEach((elemento, index)=>{
+    elemento.addEventListener('mouseout', (evento, elemento,) => {
+        descricao.innerHTML = '/* Passe o mouse por cima de alguma habilidade para ler a descrição */';
+    })
+});
