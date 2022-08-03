9c5888d9448a2c4b995bbdb24e5a64b953fa332e
Geandson Nunes <92594492+geandson14@users.noreply.github.com>
2022-08-03 12:58:58 -0300

    03/08/2022

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function(){
         
     });
 
-    $('header a').add('#serv a').add('.copyright a').click(function(e){
+    $('header a').add('#serv a').add('.copyright a').add('.texto a').click(function(e){
         e.preventDefault();
         var id = $(this).attr('href'),
         targetOffset = $(id).offset().top,
@@ -30,16 +30,16 @@ $(function(){
     });
 })
 
-const skills = document.querySelectorAll('.hab ul li');
-const descricao = document.querySelector('.desc');
-const sobreSkill = [
-    '<p>HTML</p> <br> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p> <br>',
-    '<p>CSS</p> <br> <p>É um mecanismo para adicionar estilo a um documento web.</p> <br>',
-    '<p>JavaScript</p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
-    '<p>Styled Components</p> <br> <p>É uma biblioteca (lib) que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript.</p> <br>',
-    '<p>Git</p> <br> <p>É um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.</p> <br>',
-    '<p>Github</p> <br> <p>É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>'
-]
+// const skills = document.querySelectorAll('.hab ul li');
+// const descricao = document.querySelector('.desc');
+// const sobreSkill = [
+//     '<p>HTML</p> <br> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p> <br>',
+//     '<p>CSS</p> <br> <p>É um mecanismo para adicionar estilo a um documento web.</p> <br>',
+//     '<p>JavaScript</p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
+//     '<p>Styled Components</p> <br> <p>É uma biblioteca (lib) que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript.</p> <br>',
+//     '<p>Git</p> <br> <p>É um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.</p> <br>',
+//     '<p>Github</p> <br> <p>É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>'
+// ]
 
 skills.forEach((elemento, index)=>{
     elemento.addEventListener('mouseout', (evento, elemento,) => {
